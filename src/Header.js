ee2dd9ba708bd53aa133ff8ea067c9eb35f9771b
ozngrsc <ozangursucu@hotmail.com>
2022-04-26 16:17:04 +0300

    header design

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,9 @@
 import React from "react";
 import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import LanguageIcon from "@mui/icons-material/Language";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
 function Header() {
   return (
@@ -9,6 +13,18 @@ function Header() {
         src="https://1000logos.net/wp-content/uploads/2017/08/Airbnb-Logo.png"
         alt=""
       />
+
+      <div className="header__center">
+        <input type="text" />
+        <SearchIcon />
+      </div>
+
+      <div className="header__right">
+        <p>Become a host</p>
+        <LanguageIcon />
+        <ExpandMoreIcon />
+        <AccountCircleIcon />
+      </div>
     </div>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -2,11 +2,7 @@ import React from "react";
 import "./Home.css";
 
 function Home() {
-  return (
-    <div className="home">
-      <h1>Home component</h1>
-    </div>
-  );
+  return <div className="home">{/*  <h1>Home component</h1> */}</div>;
 }
 
 export default Home;
