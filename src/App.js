0defcfe55f3821c40ff33f147922ec272ae118e7
ozngrsc <ozangursucu@hotmail.com>
2022-04-26 14:15:32 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Home from "./Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>lets build airbnb-clone</h1>
+
+      <Home />
+
+      {/* Home */}
+
+      {/* Header */}
+
+      {/* Banner */}
+      {/* Search */}
+
+      {/* Card */}
+
+      {/* Footer */}
+
+      {/* Search Page */}
+
+      {/* ... */}
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>Home component</h1>
+    </div>
+  );
+}
+
+export default Home;
