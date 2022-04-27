ecca87f1d8628500919f031bfc16d3302ddd0c54
ozngrsc <ozangursucu@hotmail.com>
2022-04-27 14:25:03 +0300

    search dates

--- src/Search.js
+++ src/Search.js
@@ -1,10 +1,30 @@
 import React from "react";
 import "./Search.css";
+import "react-date-range/dist/styles.css"; /* main style file */
+import "react-date-range/dist/theme/default.css"; /* theme css file */
+import { DateRangePicker } from "react-date-range";
+import { useState } from "react";
+
+/* Date Picker Component */
 
 function Search() {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: "selection",
+  };
+
+  function handleSelect(ranges) {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  }
+
   return (
     <div className="search">
-      <h1> Search</h1>
+      <DateRangePicker ranges={[selectionRange]} onChange={handleSelect} />
     </div>
   );
 }
