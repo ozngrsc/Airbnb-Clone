76ef389c9c06a2b4f55e1f412eba4c117b9cba5c
ozngrsc <ozangursucu@hotmail.com>
2022-04-27 13:06:45 +0300

    search

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./Banner.css";
 import { Button } from "@mui/material";
+import Search from "./Search";
 
 function Banner() {
   const [showSearch, setShowSearch] = useState(false);
@@ -8,7 +9,7 @@ function Banner() {
   return (
     <div className="banner">
       <div className="banner__search">
-        {showSearch && <h1>SHOW DATE PICKER</h1>}
+        {showSearch && <Search />}
         <Button
           onClick={() => setShowSearch(!showSearch)}
           className="banner__searchButton"
---
+++ src/Search.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Search.css";
+
+function Search() {
+  return (
+    <div className="search">
+      <h1> Search</h1>
+    </div>
+  );
+}
+
+export default Search;
