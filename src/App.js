64e4f2e300e8e61e0e4e73280486a99e1e57bdcb
ozngrsc <ozangursucu@hotmail.com>
2022-04-28 10:43:47 +0300

    footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import Footer from "./Footer";
 import Header from "./Header";
 import Home from "./Home";
 
@@ -8,6 +9,7 @@ function App() {
     <div className="app">
       <Header />
       <Home />
+      <Footer />
 
       {/* Home */}
 
